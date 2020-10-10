00e4b83553a69a389b76320d458624e27d62d81f
Pham Thanh Vinh <56020046+vinhnhibinh@users.noreply.github.com>
2020-10-10 21:49:54 +0700

    add test

---
+++ test.cpp
@@ -0,0 +1,19 @@
+/*     __//
+cf  /.__.\
+    \ \/ /
+ '__/    \
+  \-      )
+   \_____/
+_____|_|____
+     " " */
+#include <bits/stdc++.h>
+#define task "test"
+#define endl '\n'
+using namespace std;
+int main()
+{
+    freopen(task".inp","r",stdin);
+    freopen(task".out","w",stdout);
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    return 0;
+}
